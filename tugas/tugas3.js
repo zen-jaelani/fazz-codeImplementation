d09b7d6681c0d6fa3daed42c6a91e90164444dd9
Qxtlp <xi.tulop@yandex.com>
2022-03-21 10:48:55 +0700

    more readable formatting

--- tugas/tugas3.js
+++ tugas/tugas3.js
@@ -1,11 +1,11 @@
 fazzFood(75000, "FAZZFOOD50", 5, true);
 
 function fazzFood(harga, voucher, jarak, pajak) {
-	let diskon = cekDiskon(harga, voucher);
-	let ongkir = cekOngkir(jarak);
-	let tax = cekPajak(pajak, harga);
-	let total = harga - diskon + ongkir + tax;
-	console.log(`
+    const diskon = cekDiskon(harga, voucher);
+    const ongkir = cekOngkir(jarak);
+    const tax = cekPajak(pajak, harga);
+    const total = harga - diskon + ongkir + tax;
+    console.log(`
         Harga       = ${harga}
         Potongan    = ${diskon}
         Biaya antar = ${ongkir}
@@ -15,21 +15,29 @@ function fazzFood(harga, voucher, jarak, pajak) {
 }
 
 function cekDiskon(harga, voucher) {
-	let diskon;
-	if (voucher === "FAZZFOOD50" && harga >= 50000) {
-		diskon = harga - 0.5 * harga;
-		diskon = diskon > 50000 ? 50000 : diskon;
-	} else if (voucher === "DITRAKTIR60" && harga >= 25000) {
-		diskon = harga - 0.6 * harga;
-		diskon = diskon > 30000 ? 30000 : diskon;
-	} else {
-		diskon = 0;
-	}
-	return diskon;
+    let diskon = 0;
+    if (voucher === "FAZZFOOD50" && harga >= 50000) {
+        diskon = harga - 0.5 * harga;
+        diskon =
+            diskon > 50000
+                ? 50000
+                : diskon;
+    } else if (voucher === "DITRAKTIR60" && harga >= 25000) {
+        diskon = harga - 0.6 * harga;
+        diskon =
+            diskon > 30000
+                ? 30000
+                : diskon;
+    }
+    return diskon;
 }
 function cekOngkir(jarak) {
-	return jarak > 2 ? 5000 + 3000 * (jarak - 2) : 5000;
+    return jarak > 2
+        ? 5000 + 3000 * (jarak - 2)
+        : 5000;
 }
 function cekPajak(pajak, harga) {
-	return pajak ? 0.05 * harga : 0;
+    return pajak
+        ? 0.05 * harga
+        : 0;
 }
